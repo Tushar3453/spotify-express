4ca53b394e9107788865e5f056a5732e31c3aab9
pranshu423 <157400211+pranshu423@users.noreply.github.com>
2025-09-26 09:42:24 +0530

    Update server.js
    dotenv file

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config(); //.env files updated
 const spotifyRoutes = require('./routes/spotifyRoutes');
 const app = express();
 const PORT = process.env.PORT || 8000;
